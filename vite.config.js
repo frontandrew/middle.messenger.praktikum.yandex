73bf81756c60dc82588d01d957ce8db170ecd8ac
AndrewBubnov <jackblack.nt@gmail.com>
2023-12-24 00:38:47 +0700

    build(vite): entry point change

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-  plugins: [handlebars()],
+  plugins: [handlebars({
+    partialDirectory: resolve(__dirname, 'src')
+  })],
 }) 
