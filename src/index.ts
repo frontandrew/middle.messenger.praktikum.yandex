8376a290f684c5afa116a235ad8034541d4254bf
AndrewBubnov <jackblack.nt@gmail.com>
2023-12-24 15:27:31 +0700

    feat(project): create entry point

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
+import Handlebars from 'handlebars';
+import * as Pages from './pages';
+import * as Components from './components';
 
+const pages = {
+  'login': [Pages.LoginPage, { test: 'Its alive!!', label: 'Lable', error: 'FUKC' }],
+};
+
+Object.entries(Components).forEach(([name, component]) => {
+  Handlebars.registerPartial(name, component)
+})
+
+function navigate(page: string) {
+  // @ts-ignore
+  const [source, context] = pages[page];
+  const container = document.getElementById('app');
+  container.innerHTML = Handlebars.compile(source)(context);
+}
+
+document.addEventListener('DOMContentLoaded', () => navigate('login'));
+
+document.addEventListener('click', event => {
+  // @ts-ignore
+  const page = event.target.getAttribute('page');
+
+  if (page) {
+    navigate(page);
+
+    event.preventDefault();
+    event.stopImmediatePropagation();
+  }
+});
