45ce2441c087c4c8575dd1876950054d4f239368
AndrewBubnov <jackblack.nt@gmail.com>
2023-12-25 12:40:19 +0700

    feat(build): serve app on port 4000

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { resolve } from "path";
 import handlebars from "vite-plugin-handlebars";
 
 export default defineConfig({
+  server: {
+    port: 4000,
+  },
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, 'src')
