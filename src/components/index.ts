5711534604b91861eb22ea4c018692026434da14
AndrewBubnov <jackblack.nt@gmail.com>
2023-12-26 16:07:05 +0700

    feat(project): create text component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,4 @@ export { BaseLayout } from './base-layout';
 export { Button } from './button';
 export { Field } from './field';
 export { FormAuth } from './form-auth';
-export { Typography } from './typography';
+export { Text } from './text';
