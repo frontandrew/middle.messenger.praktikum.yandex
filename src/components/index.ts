4379fa6dd7c8d318e8e9b47fa93968511c158b0b
AndrewBubnov <jackblack.nt@gmail.com>
2023-12-27 15:47:49 +0700

    feat(project): create form component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 export { BaseLayout } from './base-layout';
 export { Button } from './button';
 export { Field } from './field';
+export { Form } from './form';
+export { FieldsContainer } from './form/fields-container';
 export { FormAuth } from './form-auth';
 export { FormReg } from './form-reg';
 export { Text } from './text';
