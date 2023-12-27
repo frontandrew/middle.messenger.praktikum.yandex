f6126334f9e2e64db924947e58a2e1a48e4e8d51
AndrewBubnov <jackblack.nt@gmail.com>
2023-12-27 13:48:46 +0700

    feat(project): create reg form

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export { BaseLayout } from './base-layout';
 export { Button } from './button';
 export { Field } from './field';
 export { FormAuth } from './form-auth';
+export { FormReg } from './form-reg';
 export { Text } from './text';
