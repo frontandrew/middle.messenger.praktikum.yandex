121384a356c1644557f3e2636d04a668daa75d3f
AndrewBubnov <jackblack.nt@gmail.com>
2024-01-30 17:55:01 +0500

    feat(project): create action container elem

---
+++ src/components/form/actions-container/index.ts
@@ -0,0 +1,5 @@
+/**
+ * кажется что это неотемлемый элемент формы поэтому положил в форму
+ * и импортировал из нее же
+*/
+export { default as ActionsContainer } from './actions-container.hbs?raw';
--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,2 +1,3 @@
 export { default as Form } from './form.hbs?raw';
 export { default as FieldsContainer } from './fields-container/fields-container.hbs?raw';
+export { default as ActionsContainer } from './actions-container/actions-container.hbs?raw';
