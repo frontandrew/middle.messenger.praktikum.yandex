80cc9664e490b686a517618bebe0ba02b4146f1a
AndrewBubnov <jackblack.nt@gmail.com>
2024-02-15 18:07:21 +0500

    feat: clarified partials dir

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   },
   plugins: [
     handlebars({
-      partialDirectory: resolve(__dirname, 'src')
+      partialDirectory: resolve(__dirname, 'src/components')
     }),
   ],
 });
