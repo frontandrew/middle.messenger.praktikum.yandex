2edcfb58cee4753722c20872a971809b48b91a2d
AndrewBubnov <jackblack.nt@gmail.com>
2024-03-24 08:32:44 +0500

    chore: remove partials plugin from vite conf

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
-
-import handlebars from 'vite-plugin-handlebars';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   server: { port: 3000 },
-  plugins: [
-    handlebars({ partialDirectory: resolve(__dirname, 'src/shared/ui') }),
-    tsconfigPaths(),
-  ],
+  plugins: [tsconfigPaths()],
 });
