d628de06d14e659514823221b890c3303db321e8
AndrewBubnov <jackblack.nt@gmail.com>
2024-03-27 10:51:18 +0500

    chore: some typing advantures

--- src/pages/login/components/layout/index.ts
+++ src/pages/login/components/layout/index.ts
@@ -1,9 +1,10 @@
 import { Component } from 'core';
+import type { Props } from 'core';
 
 import template from './template.hbs?raw';
 import './style.css';
 
-export class Layout extends Component {
+export class Layout extends Component<Props> {
   render() {
     return template;
   }
