0044a496c080d2d1cdd6893576443bc8a9033062
AndrewBubnov <jackblack.nt@gmail.com>
2024-04-02 22:11:43 +0500

    chore: core typing adventures

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,14 @@
-export * from './chat';
-export * from './error';
-export * from './login';
-export * from './reg';
-export * from './user';
+// export * from './chat';
+import { PageError } from './error';
+import { PageLogin } from './login';
+import { PageReg } from './reg';
+import { PageUser } from './user';
+
+export type PagesType = typeof PageError | typeof PageLogin | typeof PageReg | typeof PageUser;
+
+export const Pages = {
+  PageError,
+  PageLogin,
+  PageReg,
+  PageUser,
+};
