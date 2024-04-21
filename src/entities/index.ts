87ad16d6e3f7e1aba718ce1010920bc68d72b60c
AndrewBubnov <jackblack.nt@gmail.com>
2024-04-21 20:04:18 +0500

    feat: create entities

---
+++ src/entities/index.ts
@@ -0,0 +1,3 @@
+export type { ChatType } from './chat';
+export type { UserType } from './user';
+export type { MassageType } from './message';
---
+++ src/entities/message/type.ts
@@ -0,0 +1,8 @@
+export interface MessageType {
+  id: number,
+  text?: string,
+  media?: string,
+  type?: string,
+  userId?: number,
+  date?: string,
+}
