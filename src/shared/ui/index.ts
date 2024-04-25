6ad0c11659f2b6767158517303813755899fbd4a
AndrewBubnov <jackblack.nt@gmail.com>
2024-04-25 15:19:14 +0500

    feat: create menu component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,27 +1,27 @@
-import { Avatar } from './avatar';
-import { Button } from './button';
-import { ButtonIcon } from './button-icon';
-import { Dialog } from './dialog';
-import { Field } from './field';
-import { Form } from './form';
-import { Input } from './input';
-import { InputField } from './input-field';
-import { InputFile } from './input-file';
-import { Text } from './text';
+// import { Avatar } from './avatar';
+// import { Button } from './button';
+// import { ButtonIcon } from './button-icon';
+// import { Dialog } from './dialog';
+// import { Field } from './field';
+// import { Form } from './form';
+// import { Input } from './input';
+// import { InputField } from './input-field';
+// import { InputFile } from './input-file';
+// import { Text } from './text';
 
-// Этот тип скорее всего не нужен, эксперимент
+// // Этот тип скорее всего не нужен, эксперимент
 
-export type ComponentsType = typeof Avatar
-  | typeof Button
-  | typeof ButtonIcon
-  | typeof Dialog
-  | typeof Field
-  | typeof Form
-  | typeof Input
-  | typeof InputFile
-  | typeof InputField
-  | typeof Text
-  | typeof undefined
+// export type ComponentsType = typeof Avatar
+//   | typeof Button
+//   | typeof ButtonIcon
+//   | typeof Dialog
+//   | typeof Field
+//   | typeof Form
+//   | typeof Input
+//   | typeof InputFile
+//   | typeof InputField
+//   | typeof Text
+//   | typeof undefined
 
 export { Avatar } from './avatar';
 export { Button } from './button';
@@ -32,6 +32,7 @@ export { Form } from './form';
 export { Input } from './input';
 export { InputFile } from './input-file';
 export { InputField } from './input-field';
+export { Menu } from './menu';
 export { MenuItem } from './menu-item';
 export { Text } from './text';
 
@@ -46,5 +47,6 @@ export type { FieldArgs, FieldChildren, FieldProps } from './field/type';
 export type { InputChildren, InputProps, InputType } from './input';
 export type { InputFieldChildren, InputFieldProps } from './input-field';
 export type { InputFileChildren, InputFileProps } from './input-file';
+export type { MenuChildren, MenuProps } from './menu';
 export type { MenuItemChildren, MenuItemProps } from './menu-item';
 export type { TextProps } from './text/type';
