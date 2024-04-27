78cf2b00cd5f9dc21b5ea856c7d8d2c1635fa157
AndrewBubnov <jackblack.nt@gmail.com>
2024-04-27 12:00:32 +0500

    typing: login page

--- src/pages/login/components/layout-auth/type.ts
+++ src/pages/login/components/layout-auth/type.ts
@@ -3,11 +3,6 @@ import type { Children } from 'core';
 import { Text } from 'ui';
 
 import { FormAuth } from '../form-auth';
-import type { FormAuthData } from '../form-auth/type';
-
-export interface LayoutAuthArgs {
-  data: FormAuthData,
-}
 
 export interface LayoutAuthChildren extends Children {
   form: FormAuth,
