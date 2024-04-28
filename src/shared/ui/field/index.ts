f0922fd6e62847a9cbc8147e7462396b7de6527c
AndrewBubnov <jackblack.nt@gmail.com>
2024-04-28 18:52:19 +0500

    refact: different field layouts is parts of field component

--- src/shared/ui/field/index.ts
+++ src/shared/ui/field/index.ts
@@ -1,39 +1,60 @@
-import { Input, InputField, InputFile, InputProps } from 'ui';
 import { Component } from 'core';
-
-import type { ValidationState } from 'tools';
+import { Input } from 'ui';
 import { validators } from 'tools';
 
+import type { ValidationState } from 'tools';
+
 import type { FieldChildren, FieldProps } from './type';
-import template from './template.hbs?raw';
+import field from './template-field.hbs?raw';
+import file from './template-file.hbs?raw';
+import simple from './template-simple.hbs?raw';
 import './style.css';
 
 export class Field extends Component<FieldChildren, FieldProps> {
   constructor({
     name,
-    classes,
-    validator = validators[name] || validators.isRequired,
+    type,
     onBlur = () => this.handleValidation(),
+
+    classes,
+    hasError,
+    inline,
+    label,
+    required,
+    textError,
+    textHelp,
+    validator = validators[name] || validators.isRequired,
 
     ...rest
   }: FieldProps) {
     super({
       classes,
+      hasError,
+      inline,
+      label,
+      required,
+      textError,
+      textHelp,
       validator,
 
-      input: new Input({ name, onBlur, ...rest }),
+      name,
+      type,
+      input: new Input({
+        name,
+        type,
+        onBlur,
+        ...rest,
+      }),
     } as FieldProps & FieldChildren);
   }
 
   handleValidation() {
-    console.log('call handle validate');
     const { hasError, textError, value } = this.validate();
-    this.setProps({ hasError });
-    this.children.input.setProps({ hasError, value, textError });
+    this.setProps({ hasError, textError });
+    this.children.input.props.value = value;
   }
 
   validate(): ValidationState {
-    console.log('call validate');
     return this.props.validator!({
       required: this.props.required,
       value: this.children.input.value,
@@ -45,12 +66,14 @@ export class Field extends Component<FieldChildren, FieldProps> {
     this.children.input.reset();
   }
 
-  render() {
-    return template;
-  }
-
   setDisabledState(state: boolean) {
     this.setProps({ disabled: state });
     this.children.input.setProps({ disabled: state });
+  }
+
+  render() {
+    if (this.props.type === 'simple') return simple;
+    if (this.props.type === 'file') return file;
+    return field;
   }
 }
