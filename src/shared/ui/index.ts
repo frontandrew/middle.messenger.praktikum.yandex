92400c27d9326db7965e2278856ce8221e4adf80
AndrewBubnov <jackblack.nt@gmail.com>
2024-04-28 18:48:36 +0500

    chore: remove unused

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,28 +1,3 @@
-// import { Avatar } from './avatar';
-// import { Button } from './button';
-// import { ButtonIcon } from './button-icon';
-// import { Dialog } from './dialog';
-// import { Field } from './field';
-// import { Form } from './form';
-// import { Input } from './input';
-// import { InputField } from './input-field';
-// import { InputFile } from './input-file';
-// import { Text } from './text';
-
-// // Этот тип скорее всего не нужен, эксперимент
-
-// export type ComponentsType = typeof Avatar
-//   | typeof Button
-//   | typeof ButtonIcon
-//   | typeof Dialog
-//   | typeof Field
-//   | typeof Form
-//   | typeof Input
-//   | typeof InputFile
-//   | typeof InputField
-//   | typeof Text
-//   | typeof undefined
-
 export { Avatar } from './avatar';
 export { Button } from './button';
 export { ButtonIcon } from './button-icon';
@@ -30,8 +5,6 @@ export { Dialog } from './dialog';
 export { Field } from './field';
 export { Form } from './form';
 export { Input } from './input';
-export { InputFile } from './input-file';
-export { InputField } from './input-field';
 export { Menu } from './menu';
 export { MenuItem } from './menu-item';
 export { Text } from './text';
@@ -45,8 +18,6 @@ export type { DialogChildren, DialogProps } from './dialog/type';
 export type { FormChildren, FormData, FormProps } from './form/type';
 export type { FieldChildren, FieldProps } from './field/type';
 export type { InputChildren, InputProps, InputType } from './input';
-export type { InputFieldChildren, InputFieldProps } from './input-field';
-export type { InputFileChildren, InputFileProps } from './input-file';
 export type { MenuChildren, MenuProps } from './menu';
 export type { MenuItemChildren, MenuItemProps } from './menu-item';
 export type { TextProps } from './text/type';
