43258c1cc73aef019786f646f5fe5877e0673dd2
AndrewBubnov <jackblack.nt@gmail.com>
2024-05-01 21:38:08 +0500

    chore: collect all tools

--- src/shared/tools/index.ts
+++ src/shared/tools/index.ts
@@ -1,6 +1,11 @@
 export { deepCopy } from './deep-copy';
 export { deepEqual } from './deep-equal';
-export { validators } from './validators';
+export { first } from './first';
+export { identity } from './identity';
+export { isEmpty } from './is-empty';
+export { last } from './last';
+export { range } from './range';
 export { registerPartials } from './register-partials';
+export { validators } from './validators';
 
 export type { ValidatorParams, ValidationState } from './validators';
