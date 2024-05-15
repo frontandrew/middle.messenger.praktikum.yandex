e7da933352fd82602958bbce720fe5328c219c01
AndrewBubnov <jackblack.nt@gmail.com>
2024-05-16 00:54:29 +0500

    feat: add auth page loader

--- src/pages/login/components/layout-auth/index.ts
+++ src/pages/login/components/layout-auth/index.ts
@@ -1,20 +1,22 @@
-import { Button, Text } from 'ui';
+import { Button, Loader, Text } from 'ui';
 import { Component } from 'core';
-import type { Props } from 'core';
 import { withRouter } from 'routing';
+import { withStore } from 'store';
 
 import { FormAuth } from '../form-auth';
-import type { FormAuthData } from '../form-auth';
 
-import type { LayoutAuthChildren } from './type';
+import type { LayoutAuthChildren, LayoutAuthProps } from './type';
 import template from './template.hbs?raw';
 import './style.css';
 
-const ComponentWithRouter = withRouter(Component);
+const ComponentWithRouter = withStore(
+  (state) => ({ isLoading: state.isLoading }),
+)(withRouter(Component));
 
-export class LayoutAuth extends ComponentWithRouter<LayoutAuthChildren, Props> {
-  constructor(data: FormAuthData) {
+export class LayoutAuth extends ComponentWithRouter<LayoutAuthChildren, LayoutAuthProps> {
+  constructor() {
     super({
+      isLoading: false,
       title: new Text({
         classes: 'layout-auth__title',
         tag: 'h1',
@@ -25,8 +27,9 @@ export class LayoutAuth extends ComponentWithRouter<LayoutAuthChildren, Props> {
         variant: 'link',
         onClick: () => this.router.go('/sing-up'),
       }),
-      form: new FormAuth(data),
-    } as LayoutAuthChildren);
+      form: new FormAuth(),
+      loader: new Loader(),
+    } as LayoutAuthChildren & LayoutAuthProps);
   }
 
   render() {
--- src/pages/login/components/layout-auth/type.ts
+++ src/pages/login/components/layout-auth/type.ts
@@ -1,11 +1,15 @@
-import { Button, Text } from 'ui';
+import { Button, Loader, Text } from 'ui';
 
-import type { Children } from 'core';
+import type { Children, Props } from 'core';
 
 import { FormAuth } from '../form-auth';
 
+export interface LayoutAuthProps extends Props {
+  isLoading: boolean;
+}
 export interface LayoutAuthChildren extends Children {
-  form: FormAuth,
-  title: Text,
-  redirect: Button,
+  form: FormAuth;
+  title: Text;
+  redirect: Button;
+  loader: Loader;
 }
