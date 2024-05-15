9afab57a89d61e4dbcc27ba893dc045c90bed7ed
AndrewBubnov <jackblack.nt@gmail.com>
2024-05-16 02:28:06 +0500

    chore: clean unuses

--- src/pages/login/index.ts
+++ src/pages/login/index.ts
@@ -2,15 +2,6 @@ import { LayoutAuth } from './components';
 
 import type { FormAuthData as PageLoginContext } from './components/form-auth/type';
 
-const data = {
-  login: 'And',
-  password: '!Q1gsdgr',
-};
-
-export class PageLogin extends LayoutAuth {
-  constructor() {
-    super(data);
-  }
-}
+export class PageLogin extends LayoutAuth {}
 
 export type { PageLoginContext };
