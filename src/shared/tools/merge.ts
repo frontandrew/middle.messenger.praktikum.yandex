c9afe804af66361ab088ae8fd89cd753186c6497
AndrewBubnov <jackblack.nt@gmail.com>
2024-05-18 04:37:44 +0500

    chore: use lesson merge tool implementation

--- src/shared/tools/merge.ts
+++ src/shared/tools/merge.ts
@@ -1,17 +1,39 @@
+/* eslint-disable */
+/* TODO: this tool must be refactored!! */
 export function merge(lhs: PlainObject, rhs: PlainObject): PlainObject {
-  if ((typeof lhs !== 'object' || lhs === null) && rhs !== undefined) return rhs;
-  if (typeof lhs !== 'object') return lhs;
+  for (const p in rhs) {
+    if (!rhs.hasOwnProperty(p)) {
+      continue;
+    }
 
-  const res = Object.entries(rhs)
-    .reduce((result, [key, value]) => {
-      if (key in result) {
-        return {
-          ...result,
-          [key]: merge(lhs[key], rhs[key]),
-        };
+    try {
+      if (rhs[p].constructor === Object) {
+        rhs[p] = merge(lhs[p] as PlainObject, rhs[p] as PlainObject);
+      } else {
+        lhs[p] = rhs[p];
       }
-      return { ...result, [key]: value };
-    }, lhs);
+    } catch (e) {
+      lhs[p] = rhs[p];
+    }
+  }
 
-  return res;
+  return lhs;
 }
+
+// export function merge(lhs: PlainObject, rhs: PlainObject): PlainObject {
+//   if ((typeof lhs !== 'object' || lhs === null) && rhs !== undefined) return rhs;
+//   if (typeof lhs !== 'object') return lhs;
+
+//   const res = Object.entries(rhs)
+//     .reduce((result, [key, value]) => {
+//       if (key in result) {
+//         return {
+//           ...result,
+//           [key]: merge(lhs[key], rhs[key]),
+//         };
+//       }
+//       return { ...result, [key]: value };
+//     }, lhs);
+
+//   return res;
+// }
