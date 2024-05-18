50b75742dbf94a5f7af3cc79222513f0acf167b9
AndrewBubnov <jackblack.nt@gmail.com>
2024-05-18 10:56:59 +0500

    feat: user avatar with data

--- src/pages/user/components/control-avatar/index.ts
+++ src/pages/user/components/control-avatar/index.ts
@@ -1,18 +1,22 @@
 import { Avatar } from 'ui';
 import { Component } from 'core';
+import { withStore } from 'store';
 
 import type { ControlAvatarChildren, ControlAvatarProps } from './type';
 import template from './template.hbs?raw';
 import './style.css';
 
+const AvatarWithState = withStore(
+
+  /* TODO: move resorce endpoit to constants */
+  (state) => ({ pic: `https://ya-praktikum.tech/api/v2/resources${state.user?.image}` }),
+)(Avatar);
+
 export class ControlAvatar extends Component <ControlAvatarChildren, ControlAvatarProps> {
-  constructor({ image, ...rest }: ControlAvatarProps) {
+  constructor(props: ControlAvatarProps) {
     super({
-      avatar: new Avatar({
-        pic: image,
-        size: 'large',
-      }),
-      ...rest,
+      avatar: new AvatarWithState({ size: 'large' }),
+      ...props,
     } as ControlAvatarChildren & ControlAvatarProps);
   }
 
