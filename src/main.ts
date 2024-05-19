8a1ede0463d98ddb735052b3e5b8e8dfef4d8c95
AndrewBubnov <jackblack.nt@gmail.com>
2024-05-20 01:20:10 +0500

    fix: app controller top level await

--- src/main.ts
+++ src/main.ts
@@ -24,11 +24,7 @@ const app = new App();
 root?.appendChild(app.getContent()!);
 
 const controller = new AppController();
-/**
- * TODO: Top-level await is not available in the configuredtarget environment
- * ("chrome87", "edge88", "es2020", "firefox78", "safari14" + 2 overrides)
- */
-await controller.start();
+(async () => { await controller.start(); })();
 
 /* IMPORTANT: start router after App render */
 
