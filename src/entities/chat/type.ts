804dda8e1e7e3ed2b60362fc3b8d80253edf5f06
AndrewBubnov <jackblack.nt@gmail.com>
2024-05-19 07:42:07 +0500

    chore: chat entity typing

--- src/entities/chat/type.ts
+++ src/entities/chat/type.ts
@@ -1,8 +1,37 @@
 export interface ChatType {
-  id: number,
-  name: string,
-  avatar?: string,
-  lastMessage?: string,
-  date?: string,
-  count?: number,
+  id: number;
+  title: string;
+  avatar: string | null;
+  unreadCount: number;
+  authorId: number
+  lastMessage: string | null;
+  time: string | null;
+}
+
+export interface ListChatsPayload {
+  offset?: number;
+  limit?: number;
+  title?: string;
+}
+
+export type ListChatsResponse = ChatResponse[];
+
+export interface ChatResponse {
+  id: number;
+  title: string;
+  avatar: string | null;
+  unread_count: number;
+  created_by: number
+  last_message: {
+    user: {
+      first_name: string;
+      second_name: string;
+      avatar: string;
+      email: string;
+      login: string;
+      phone: string;
+    },
+    time: string;
+    content: string;
+  } | null;
 }
