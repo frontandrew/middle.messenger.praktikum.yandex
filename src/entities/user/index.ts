ebc0aaa665aa8d3e081c60bb8a94ac69a35f0e4a
AndrewBubnov <jackblack.nt@gmail.com>
2024-05-23 17:13:54 +0500

    feat: create item user ui

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -1,3 +1,4 @@
+export type { ItemUserChildren, ItemUserKeyAttr, ItemUserProps } from './ui';
 export type { UserType } from './type';
 
 export { ItemUser } from './ui';
--- src/entities/user/ui/index.ts
+++ src/entities/user/ui/index.ts
@@ -1,1 +1,2 @@
+export type { ItemUserChildren, ItemUserKeyAttr, ItemUserProps } from './item-user/type';
 export { ItemUser } from './item-user';
