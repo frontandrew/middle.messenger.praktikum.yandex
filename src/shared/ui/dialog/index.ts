11fced9bc3810776bf5c9879018ccc762e253e0e
AndrewBubnov <jackblack.nt@gmail.com>
2024-05-24 01:57:59 +0500

    feat: make more universal ui/dialog

--- src/shared/ui/dialog/index.ts
+++ src/shared/ui/dialog/index.ts
@@ -4,25 +4,27 @@ import type { DialogChildren, DialogProps } from './type';
 import template from './template.hbs?raw';
 import './style.css';
 
-export class Dialog extends Component<DialogChildren, DialogProps> {
-  constructor({ isOpen = false, ...rest }) {
+export class Dialog<C extends DialogChildren, P extends DialogProps> extends Component<C, P> {
+  constructor({ isOpen = false, onClick }: DialogProps) {
     super({
       isOpen,
-      onClick: () => this.close(),
-      ...rest,
-    } as DialogChildren & DialogProps);
+      onClick: () => {
+        if (onClick) onClick();
+        this.close();
+      },
+    } as C & P);
   }
 
   public setVisibility(state: boolean) {
-    this.setProps({ isOpen: state });
+    this.setProps({ isOpen: state } as P);
   }
 
   public close() {
-    this.setProps({ isOpen: false });
+    this.setProps({ isOpen: false } as P);
   }
 
   public open() {
-    this.setProps({ isOpen: true });
+    this.setProps({ isOpen: true } as P);
   }
 
   render() {
--- src/shared/ui/dialog/type.ts
+++ src/shared/ui/dialog/type.ts
@@ -1,10 +1,6 @@
-import { Form } from 'ui';
-
 import type { Children, Props } from 'core';
 
-export interface DialogChildren extends Children {
-  content: InstanceType<typeof Form>,
-}
+export interface DialogChildren extends Children {}
 
 export interface DialogProps extends Props {
   isOpen: boolean,
