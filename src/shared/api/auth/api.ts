8f5fd385ecf7c75c1d91644e0a22677cc24487a1
AndrewBubnov <jackblack.nt@gmail.com>
2024-05-25 14:58:51 +0500

    refact: shared/api/auth

--- src/shared/api/auth/api.ts
+++ src/shared/api/auth/api.ts
@@ -1,25 +1,24 @@
 import { HTTPTransport } from 'network';
-
-import type { UserResponse } from 'services/users';
+import type{ UserResponse } from 'api/user';
 
 import type { AuthPayload, AuthResponse, RegPayload, RegResponse } from './type';
 
 export class AuthAPI {
-  private api = new HTTPTransport();
+  private http = new HTTPTransport();
 
   public login(payload: AuthPayload) {
-    return this.api.post<AuthPayload, AuthResponse>('/auth/signin', { data: payload });
+    return this.http.post<AuthPayload, AuthResponse>('/auth/signin', { data: payload });
   }
 
   public logout() {
-    return this.api.post<void, AuthResponse>('/auth/logout');
+    return this.http.post<void, AuthResponse>('/auth/logout');
   }
 
   public getUserData() {
-    return this.api.get<void, UserResponse>('/auth/user');
+    return this.http.get<void, UserResponse>('/auth/user');
   }
 
   public registration(data: RegPayload) {
-    return this.api.post<RegPayload, RegResponse>('/auth/signup', { data });
+    return this.http.post<RegPayload, RegResponse>('/auth/signup', { data });
   }
 }
