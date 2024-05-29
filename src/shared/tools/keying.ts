0335d88e0f49447e53fd4f63f1e243fcc7dfa0c1
AndrewBubnov <jackblack.nt@gmail.com>
2024-05-29 14:00:13 +0500

    fix: keying tool more predictable

--- src/shared/tools/keying.ts
+++ src/shared/tools/keying.ts
@@ -4,15 +4,18 @@ import { isArray } from './is-array';
  * Transforms an array of objects into a dictionary keyed by a specified property.
  * If receive not an array param, it simple return firsn argument
  *
- * @param array a list of objects.
+ * @param array an array list of objects.
  *
  * @param key the property to use as the key for the dictionary
  *
  * @returns array of objecst, signed by 'key' param value
  */
 
-export function keying<T>(array: T[], key: keyof T): { [key: string]: T } | T[] {
-  if (!isArray) return array;
+export function keying<T>(array: T[], key: keyof T): { [key: string]: T } {
+  if (!isArray) return {};
   return array
-    .reduce((res, item) => ({ ...res, [`${item[key]}`]: item }), {});
+    .reduce((res, item) => {
+      if (!['number', 'string'].includes(typeof item[key])) return res;
+      return ({ ...res, [`${item[key]}`]: item });
+    }, {});
 }
